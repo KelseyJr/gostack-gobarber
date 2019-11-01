e74af0883f2a11a5855a899483e506e5466b5371
Kelsey Junior <kelseytfjunior@gmail.com>
2019-11-01 18:16:19 -0300

    Nodemailer config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fdcee86e5d4911',
+    pass: '5ff75ec30247d1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
